1384d20cf469cec044cb614be75d371f444110d7
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2024-10-07 00:59:00 +0300

    commit

--- src/pages/api/tinymce.ts
+++ src/pages/api/tinymce.ts
@@ -2,8 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log('hendler')
-    const response = await fetch('https://tinymceapikey-jme-7y3mjja-uc.a.run.app'); // Replace with your function URL
-    const data = await response.json();
+    const response = await fetch('https://us-central1-baatbgorg.cloudfunctions.net/tinymceApiKey'); // Replace with your function URL
+    // const data = await response.json();
+    console.log(response)
 
-    res.status(200).json(data);
+    // res.status(200).json(data);
 }
