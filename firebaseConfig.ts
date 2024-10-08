141550173fc55280f5671cf90d019846f9136ce2
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2024-10-08 20:24:56 +0300

    adds firebase config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,9 +1,17 @@
 // firebaseConfig.ts
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // your config here
+  apiKey: "AIzaSyAWmDho7AeyXNJXgY1tsu6twXh9sPg27qA",
+  authDomain: "baatbgorg.firebaseapp.com",
+  databaseURL:
+    "https://baatbgorg-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "baatbgorg",
+  storageBucket: "baatbgorg.appspot.com",
+  messagingSenderId: "173955335103",
+  appId: "1:173955335103:web:2043ff36eaa0502236970a",
+  measurementId: "G-L97SQVHZ2X",
 };
 
 const app = initializeApp(firebaseConfig);
