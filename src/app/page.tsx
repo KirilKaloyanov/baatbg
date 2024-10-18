ad727c85d9ecf48a192f17f667b62535f47a4b28
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2024-10-18 11:06:04 +0300

    new tiny api key from prod env var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 
-
+import functions from "firebase-functions"
 import dynamic from "next/dynamic";
 
 export default async function Page() {
 
   const TinyMCEditor = dynamic(() => import("../components/richEditor"), { ssr: false })
 
-  let apiKey = process.env.LOCAL_TINYMCE_API_URL;
+  // console.log(process.env.TINYMCE_API_KEY)
 
-  if (!apiKey) {
-    try {
-      const response = await fetch("https://tinymceapikey-jme7y3mjja-uc.a.run.app"); 
-      const  data  = await response.json();
-      apiKey = data.apiKey;
-    } catch (error) {
-      console.error("Error fetching TinyMCE API Key:", error)
-    }
-  }
+  let apiKey = process.env.TINYMCE_API_KEY;
+
+  // if (!apiKey) {
+  //   try {
+  //     const response = await fetch("https://tinymceapikey-jme7y3mjja-uc.a.run.app"); 
+  //     const  data  = await response.json();
+  //     apiKey = data.apiKey;
+  //   } catch (error) {
+  //     console.error("Error fetching TinyMCE API Key:", error)
+  //   }
+  // }
 
   if (!apiKey) return <h1> Waiting for editor key</h1>;
   if (apiKey)
