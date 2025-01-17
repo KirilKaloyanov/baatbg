3a4affdb63cbb073b70f2e4790da75c410d9567b
Kiril Kaloyanov <kiril.kaloyanov@dxc.com>
2025-01-17 15:17:31 +0200

    removes dashboard content

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,7 @@ import Link from "next/link";
 
 export default async function DashBoard() {
     return ( 
-    <>    
-    <Link href={"/dashboard/menu"}>Edit Menus</Link>    
-    <Link href={"/dashboard/posts"}>Edit Posts</Link>    
+    <>     
     </>
     )
 }
