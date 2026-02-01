9331fd78550c3776494d2d1c76c8d7b4e83f302f
Kiril <kaloyanov.kiril@gmail.com>
2026-02-01 12:41:50 +0200

    Simplify middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -8,33 +8,12 @@ const intlMiddleware = createMiddleware(routing);
 
 export default function proxy(request: NextRequest) {
   
-  const host = request.headers.get("host");
-  const targetDomain = "baat-api--baatbgorg.us-central1.hosted.app";
-
-  // Redirect if accessed via the internal .run.app 
-  if (host && host.includes(".a.run.app")) {
-    const url = request.nextUrl.clone();
-    url.host = targetDomain;
-    url.protocol = "https";
-    return NextResponse.redirect(url, 301);
-  }
-
   if (request.nextUrl.pathname === "/") {
     const acceptLang = request.headers.get("accept-language")?.split(",")[0];
     const preferredLocale = acceptLang?.startsWith("bg") ? "bg" : "en";
     return NextResponse.redirect(new URL(`/${preferredLocale}`, request.url));
   }
   
-  const [_, locale, ...pathSegments] = request.nextUrl.pathname.split('/');
-
-  const currentLocale = locale && (locale === 'bg' || locale === 'en') ? locale : "en";
-  
-  const path = pathSegments.join('/');
-  // console.log(currentLocale, path)
-  // if (path === 'projects') {
-  //   return NextResponse.redirect(new URL(`/${currentLocale}/projects/danube-cycle-plans`, request.url));
-  // }
-
   return intlMiddleware(request);
 }
 
@@ -42,7 +21,5 @@ export const config = {
   matcher: [
     "/",
     "/(en|bg)/:path*",
-    // '/((?!_next|_vercel|.*\\..*).*)'
-    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt).*)",
   ],
 };
